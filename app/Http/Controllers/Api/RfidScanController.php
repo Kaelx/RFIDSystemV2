4d9d4310baf43a54835a7fe62daca580c8761a5a
Kaelx <johnmark.cuyos@evsu.edu.ph>
2026-01-28 07:16:50 +0800

    Implement RFID search functionality in rfidRecord method

--- app/Http/Controllers/Api/RfidScanController.php
+++ app/Http/Controllers/Api/RfidScanController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\RfidScan;
+use Illuminate\Http\Request;
 
 class RfidScanController extends Controller
 {
 
-    public function rfidRecord()
+    public function rfidRecord(Request $request)
     {
+        $query = RfidScan::with('recordable');
 
-        $records = RfidScan::with('recordable')->get();
+        // Search by RFID
+        if ($request->has('rfid')) {
+            $rfid = $request->rfid;
+
+            $query->where(function ($q) use ($rfid) {
+                $q->whereHasMorph('recordable', ['App\Models\Student', 'App\Models\Employee', 'App\Models\Seller'], function ($query) use ($rfid) {
+                    $query->where('rfid', $rfid);
+                });
+            });
+        }
+
+        $records = $query->get();
 
         return response()->json([
             'success' => true,
